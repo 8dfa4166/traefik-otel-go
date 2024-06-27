011894ae9e8159af758cfc0f620ec465e6570b2a
058a <058.auriga@gmail.com>
2024-06-27 18:05:37 +0900

    propagated

--- web/app/api/hello/route.ts
+++ web/app/api/hello/route.ts
@@ -4,7 +4,13 @@ import { NextResponse } from "next/server";
 export const GET = auth(async function GET(req) {
   if (!req.auth) return NextResponse.json({ message: "Not authenticated" }, { status: 401 });
 
-  const res = await fetch("http://api:1323/hello")
+  const traceparent = req.headers.get("Traceparent")!;
+  const res = await fetch("http://api:1323/hello", {
+    method: "GET",
+    headers: {
+      "Traceparent": traceparent
+    },
+  })
   const result = await res.json()
   return NextResponse.json({message: result, auth: req.auth}, { status: 200 });
 });
