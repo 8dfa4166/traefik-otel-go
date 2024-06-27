76fb8b2d8f3edcf7e54e3839972845f4e34ca22c
058a <058.auriga@gmail.com>
2024-06-27 17:48:42 +0900

    refactored

--- web/app/api/hello/route.ts
+++ web/app/api/hello/route.ts
@@ -1,6 +1,5 @@
 import { auth } from "@/auth";
 import { NextResponse } from "next/server";
-import { trace, context, Span } from "@opentelemetry/api";
 
 export const GET = auth(async function GET(req) {
   if (!req.auth) return NextResponse.json({ message: "Not authenticated" }, { status: 401 });
