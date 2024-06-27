489f5ba92052d3d867822ec24c22402f581ee6c3
058a <058.auriga@gmail.com>
2024-06-27 15:55:21 +0900

    refactored

--- api/main.go
+++ api/main.go
@@ -50,17 +50,7 @@ func main() {
 			log.Fatalf("failed to shutdown TracerProvider: %s", err)
 		}
 	}()
-
-	// tp, err := initTracer()
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// defer func() {
-	// 	if err := tp.Shutdown(context.Background()); err != nil {
-	// 		log.Printf("Error shutting down tracer provider: %v", err)
-	// 	}
-	// }()
-
+	
 	e := echo.New()
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
@@ -73,10 +63,7 @@ func main() {
 }
 
 func initConn() (*grpc.ClientConn, error) {
-	// It connects the OpenTelemetry Collector through local gRPC connection.
-	// You may replace `localhost:4317` with your endpoint.
 	conn, err := grpc.NewClient("otelcol:4317",
-		// Note the use of insecure transport here. TLS is recommended in production.
 		grpc.WithTransportCredentials(insecure.NewCredentials()),
 	)
 	if err != nil {
@@ -87,14 +74,11 @@ func initConn() (*grpc.ClientConn, error) {
 }
 
 func initTracerProvider(ctx context.Context, res *resource.Resource, conn *grpc.ClientConn) (func(context.Context) error, error) {
-	// Set up a trace exporter
 	traceExporter, err := otlptracegrpc.New(ctx, otlptracegrpc.WithGRPCConn(conn))
 	if err != nil {
 		return nil, fmt.Errorf("failed to create trace exporter: %w", err)
 	}
 
-	// Register the trace exporter with a TracerProvider, using a batch
-	// span processor to aggregate spans before export.
 	bsp := sdktrace.NewBatchSpanProcessor(traceExporter)
 	tracerProvider := sdktrace.NewTracerProvider(
 		sdktrace.WithSampler(sdktrace.AlwaysSample()),
@@ -103,9 +87,7 @@ func initTracerProvider(ctx context.Context, res *resource.Resource, conn *grpc.
 	)
 	otel.SetTracerProvider(tracerProvider)
 
-	// Set global propagator to tracecontext (the default is no-op).
 	otel.SetTextMapPropagator(propagation.TraceContext{})
 
-	// Shutdown will flush any remaining spans and shut down the exporter.
 	return tracerProvider.Shutdown, nil
 }
